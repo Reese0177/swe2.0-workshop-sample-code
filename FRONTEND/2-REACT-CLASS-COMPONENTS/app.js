8207b1b3fe304d3d2404395d1145b05d2b4feb4d
= <90lineng@gmail.com>
2022-12-15 13:35:35 -0500

    FrontEnd - class component starter code added

---
+++ FRONTEND/2-REACT-CLASS-COMPONENTS/app.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class App extends React.Component {
+    constructor() {
+        super()
+    }
+
+    render() {
+        return(
+            <h1>TEACH ME REACT COMPONENTS PLEASE</h1>
+        )
+    }
+}
+
+export default App
