cebf94ad5b41f4bef688b2f74742a8d648978da5
= <90lineng@gmail.com>
2023-01-06 10:39:21 -0500

    demo starter code added

---
+++ FRONTEND/3-REACT-ROUTER/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+/*
+  make sure to install the latest version of react-router-dom
+  npm install react-router-dom@6
+*/
+
+function App() {
+  return (
+    <div className='mt-4 mx-auto h-75 w-75'>
+      <p>this is where we will create the routes</p>
+      
+    </div>
+  );
+}
+
+export { App };
