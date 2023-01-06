33dd30a5b7f3df38da525acf7a0efb543fc8de4e
Reese0177 <vinnygo@gmail.com>
2023-01-06 11:14:33 -0600

    Add basic routes

--- FRONTEND/3-REACT-ROUTER/src/App.js
+++ FRONTEND/3-REACT-ROUTER/src/App.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import Home from './routes/Home';
+import Blog from './routes/Blog';
+import About from './routes/About';
+
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 /*
   make sure to install the latest version of react-router-dom
@@ -9,6 +14,13 @@ function App() {
   return (
     <div className='mt-4 mx-auto h-75 w-75'>
       <p>this is where we will create the routes</p>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/about" element={<About/>} />
+          <Route path="/blog" element={<Blog/>} />
+        </Routes>
+      </BrowserRouter>
       
     </div>
   );
